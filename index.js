a6bbfcbc11186e9d176983202fde6e827e6177d1
Lukas Jakobsen <109243@RTS-365.dk>
2020-01-10 12:44:07 +0100

    push til mappen node_modules for at få gulp template

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
 var fs = require("fs");
+var path = require("path");
+var mkdir = require("mkdirp");
+var { exec } = require("child_process");
 
-fs.copyFileSync("./pkg/gulpfile.js", "./gulpfile.js")
-var tasks = fs.readdirSync("./pkg/tasks");
+var TEMPLATE_DIR = path.join(__dirname, "pkg");
+var MODE_0666 = parseInt("0666", 8);
 
-fs.mkdirSync("./tasks")
+if (!fs.existsSync("./package.json")) {
+    console.log("please run <npm init> before you run this file");
+    process.exit(1)
+}
 
-tasks.forEach(function(file){
-    fs.copyFileSync(`./pkg/tasks/${file}`, `./tasks/${file}`)
-})
+function copyFile(from, to) {
+    write(to, fs.readFileSync(path.join(TEMPLATE_DIR, from), 'utf-8'));
+}
+
+function copyFileMulti(fromDir, toDir) {
+    fs.readdirSync(path.join(TEMPLATE_DIR, fromDir)).forEach(function (file) {
+        if (fs.lstatSync(path.join(TEMPLATE_DIR, fromDir, file)).isFile()) {
+            copyFile(path.join(fromDir, file), path.join(toDir, file));
+        }
+    });
+}
+
+function write(file, str, mode) {
+    fs.writeFileSync(file, str, { mode: mode || MODE_0666 })
+    console.log('   \x1b[36mcreate\x1b[0m : ' + file);
+}
+
+mkdir.sync("./tasks");
+mkdir.sync("./src/html");
+mkdir.sync("./src/html/layouts");
+mkdir.sync("./src/scss");
+mkdir.sync("./src/js");
+mkdir.sync("./src/images");
+mkdir.sync("./src/media");
+
+copyFileMulti("tasks", "./tasks");
+copyFileMulti("src/html", "./src/html");
+copyFileMulti("src/html/layouts", "./src/html/layouts");
+copyFileMulti("src/scss", "./src/scss");
+copyFileMulti("src/js", "./src/js");
+copyFileMulti("src/images", "./src/images");
+copyFileMulti("src/media", "./src/media");
+
+copyFile("gulpfile.js", "./gulpfile.js") /*gulp file */
+
+exec("npm i -D @babel/core @babel/preset-env gulp gulp-babel gulp-clean-css gulp-concat gulp-connect gulp-declare gulp-imagemin gulp-minify gulp-pug gulp-rename gulp-sass gulp-sourcemaps gulp-terser gulp-uglify imagemin-jpeg-recompress uglify-js", function(err, stdout, stderr){
+    if(err){
+            console.log(err);
+            process.exit(1);
+    }
+
+    console.log("installing npm packages. please wait..");
+    
+    // console.log(stdout);
+    stdout.on("data", function(data){
+        console.log(data.toString());
+        
+    })
+});
